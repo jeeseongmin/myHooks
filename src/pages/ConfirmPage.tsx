b7fc1510e75c8ac20e2ee7ed26a73f18705788d4
patrick <peration0422@exntu.com>
2022-02-09 23:12:38 +0900

    useConfirm

--- src/pages/ConfirmPage.tsx
+++ src/pages/ConfirmPage.tsx
@@ -4,24 +4,26 @@ type ConfirmPageProps = {};
 
 type ConfirmPageState = {};
 
+const useConfirm = (message: string, callback: Function) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    // if (confirm(message)) {
+    //   callback(0);
+    // }
+    callback(0);
+  };
+  return confirmAction;
+};
 const ConfirmPage: FC<ConfirmPageProps> = ({}) => {
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // | State Variables
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*
-
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // | Hooks
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*
-
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // | Functions
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*
-
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // | Mark Up
-  // |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*
-
-  return <div></div>;
+  const deleteWorld = () => console.log("Deleting the World");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld);
+  return (
+    <div>
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
 };
 
 export default ConfirmPage;
